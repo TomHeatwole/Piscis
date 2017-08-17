ee2185c77eb0669f400b69f12b3488455c431363
Tom <tgh8xn@virginia.edu>
2017-08-17 03:39:00 -0400

    Editing run.sh to work for all file names (no longer need to manually update class names)

--- src/poker/nlhe2max/run.sh
+++ src/poker/nlhe2max/run.sh
@@ -2,10 +2,22 @@
 mkdir test
 cp ../../*.java test
 cp ../*.java test
-cp /*.java test
+#cp /*.java test
 cd test
 javac *.java 
-java Deck PokerPlayer Player Match
+#java PokerPlayer Player Match
+strindex() {
+    x="${1%%$2*}"
+    [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+classes=""
+for file in *.class
+do      
+    pos=$(strindex $file .class)
+    className=$( echo $file | cut -c1-$pos )
+    classes="$classes $className"
+done
+java $classes
 cd ..
 rm test/*
 rmdir test
