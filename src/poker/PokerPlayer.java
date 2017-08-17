679f67ced5f705f37462500d7d9e9664fd0c70e1
Tom <tgh8xn@virginia.edu>
2017-08-17 03:56:36 -0400

    Populating Player.java interface

--- src/poker/PokerPlayer.java
+++ src/poker/PokerPlayer.java
@@ -1,10 +1,30 @@
 public class PokerPlayer implements Player {
 
     public String name;
+    public int chipCount;
     public PokerPlayer() {
-
+        name = "";
+        chipCount = 0;
     }
+
+    public PokerPlayer(String name, int chipCount) {
+        this.name = name;
+        this.chipCount = chipCount;
+    }
+
     public String getName() {
-        return "Elvin";
+        return name;
+    }
+
+    public int getCount() {
+        return chipCount;
+    }
+
+    public int addChips(int c) {
+        chipCount += c;
+    } 
+
+    public int removeChips(int c) {
+        chipCount -= c;
     }
 }
