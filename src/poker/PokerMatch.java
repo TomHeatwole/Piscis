eafd2d767e2d1a227de57999401f3f955797de65
uvafan <uvafan173@gmail.com>
2017-08-20 18:36:44 -0400

    unfinished stuff in PokerMatch

--- src/poker/PokerMatch.java
+++ src/poker/PokerMatch.java
@@ -5,40 +5,67 @@ import java.util.*;
 public abstract class PokerMatch implements Match {
     
     private int handsRemaining;
+    private int numStreets; 
     private List<PokerPlayer> players;
     private Map<PokerPlayer,Integer> net; 
+    private Map<PokerPlayer,Integer> results;
     private Deck deck;
     private int button; // position of the button 
     private int initialChipCounts;
- 
-    public PokerMatch(int numHands, int numPlayers, int initialChipCounts) {
+    private boolean isHandOver; 
+
+    public PokerMatch(int numHands, int numPlayers, int initialChipCounts, int numStreets) {
         this.handsRemaining = numHands;
         this.deck = new Deck();
         this.net = new HashMap<PokerPlayer,Integer>();
         this.initialChipCounts = initialChipCounts; 
+        this.numStreets = numStreets;
         initializePlayers(numPlayers, initialChipCounts); 
-        play();
     }
     
     private void play() {
         button = (int)(Math.random() * players.size()); 
         while (handsRemaining > 0){
+            isHandOver = false;
             deck.shuffle();
-            Map<PokerPlayer,Integer> results = processHand(players, deck, button); 
+            for(PokerPlayer p: players)
+                results.put(p,0); 
+            for(int street = 0; street < numStreets+1; street++){
+                processStreet(street); 
+                processBetting(); 
+                if(isHandOver){
+                    break; 
+                }
+            }
+            if(!isHandOver){
+                processShowdown();
+            }
             updateNetAndResetCounts(results); 
             button = (button + 1) % players.size(); 
             handsRemaining--; 
         }
     }
     
+    private abstract int showdown(){};
+
+    //TODO: call showdown, update results
+    public void processShowdown(){
+    }
+
+    //TODO: process all betting
+    public void processBetting(){
+    }   
+
+    public abstract void processStreet(int street);    
+
     //abstract since this is where different poker variants differ 
-    public abstract Map<PokerPlayer,Integer> processHand(List<PokerPlayer> players, Deck deck, int button);
+    public abstract int[] processHand(List<PokerPlayer> players, Deck deck, int button);
 
     private void updateNetAndResetCounts(Map<PokerPlayer,Integer> results){
         for(Map.Entry<PokerPlayer,Integer> entry: results.entrySet()){
-            PokerPlayer player = entry.getKey();
+            PokerPlayer p = entry.getKey();
             int result = entry.getValue();
-            net.put(player, net.get(player) + result); 
+            net.put(p, net.get(p) + result);
         }
         for(PokerPlayer p: players){
             p.setChips(initialChipCounts);
@@ -51,6 +78,7 @@ public abstract class PokerMatch implements Match {
         } 
         for(PokerPlayer p: players){
             this.net.put(p,0); 
+            this.results.put(p,0);
         } 
     }
 
