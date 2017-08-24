45665c10f17606e39223a2f6892ed112f77aad43
Tom <tgh8xn@virginia.edu>
2017-08-24 01:41:49 -0400

    Skeleton for IO.java

--- src/poker/nlhe2max/Driver.java
+++ src/poker/nlhe2max/Driver.java
@@ -1,8 +1,6 @@
 public class Driver {
     public static void main (String[] args) {
-        int[] structure = {1,2,0};
-        String[] names = {"Eli","Tom"};
-        HoldEmMatch a = new HoldEmMatch(5,names,200,structure);
-        a.play();
+        IO i = new IO("i.txt", "o.txt");
+        i.output("monsters");
     }
 }
